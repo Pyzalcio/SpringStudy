5eab46ff71855b5ccbed0969fd01440d36f0626c
Pyzalcio <michal.pyzalski7@gmail.com>
2022-02-21 12:53:32 +0100

    Initial

---
+++ src/main/java/com/example/demo/model/Task.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+
+import com.sun.istack.NotNull;
+import org.apache.logging.log4j.message.Message;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Task's description must not be empty")
+    private String description;
+    private boolean done;
+
+    public Task()
+    {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
---
+++ src/main/java/com/example/demo/model/TaskRepository.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository
+{
+    List<Task> findAll();
+
+    Page<Task> findAll(Pageable pageable);
+
+    Optional<Task> findById(Integer I);
+
+    boolean existsById(Integer id);
+
+    List<Task> findByDone(@Param("state") boolean done);
+
+    Task save(Task entity);
+}
